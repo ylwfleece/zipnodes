879254acc91a1ca3e6ff5ccc25193aa01d491e2a
lettemax <maxlettenberger@gmail.com>
2021-03-08 16:18:26 -0600

    add filled orders to node profile

--- react-app/src/components/UserProfile/index.js
+++ react-app/src/components/UserProfile/index.js
@@ -9,15 +9,21 @@ const UserProfile = ({ authenticated, setAuthenticated }) => {
 
   const user = useSelector((state) => state.session.user);
   const params = useParams()
-  //const order = useSelector((state) => state.orders[params.id]);
   const reviews = useSelector((state) => state.reviews.list);
   const applications = useSelector((state) => state.applications.list);
 
+  let apps = [];
+  if (applications) {
+    apps = applications.filter(app => app.node_id == user.id);
+  }
+  // const applications = useSelector((state) => state.applications.list).filter(app => app.node_id == user.id && app.status == "Complete");
+
+  // const orders = useSelector((state) => state.applications.list);
   let score = 0;
   let divisor = 0;
   let karma = 0;
   if (reviews) {
-    for (let i=0; i<reviews.length; i++) {
+    for (let i = 0; i < reviews.length; i++) {
       if (reviews[i].reviewee_id == user.id) {
         score += reviews[i].score;
         if (reviews[i].score > 1) {
@@ -27,7 +33,7 @@ const UserProfile = ({ authenticated, setAuthenticated }) => {
       }
     }
     if (divisor > 0) {
-      score = Math. round(10*(score/divisor))/10;
+      score = Math.round(10 * (score / divisor)) / 10;
     }
   }
 
@@ -35,23 +41,37 @@ const UserProfile = ({ authenticated, setAuthenticated }) => {
     <div className='page-container homepage-container'>
       <div>
         <div className='container'>
-          {(user && reviews) && 
+          {(user && reviews) &&
             <>
-                <div>
-                    {user.username}
+              <div>
+                {user.username}
+              </div>
+              <div>
+                score: {score}
+              </div>
+              <div>
+                karma: {karma}
+              </div>
+            </>
+          }
+        </div>
+        <div className='container'>
+          {(user && applications) &&
+            <>
+              <div style={{ paddingBottom: '0', marginBottom: '2vh' }}># orders filled: {applications.length}</div>
+              {apps.map((app) =>
+                <div key={app.id} className='container posts' style={{ paddingTop: '0', marginBottom: '3vh' }}>
+                  <div>{app.order_title}</div>
+                  <div>{app.node.username}</div>
+                  <div>{app.order_start_time}</div>                 
                 </div>
-                <div>
-                    score: {score}
-                </div>
-                <div>
-                    karma: {karma}
-                </div>
+              )}
             </>
           }
         </div>
       </div>
     </div>
-    
+
 
   );
 };
