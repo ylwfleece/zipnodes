c1377949c58b64f26e9f73edbf34d9beec442e32
lettemax <maxlettenberger@gmail.com>
2021-03-09 17:10:56 -0600

    Update rev profile

--- react-app/src/components/ReviewProfile/index.js
+++ react-app/src/components/ReviewProfile/index.js
@@ -6,26 +6,31 @@ const ReviewProfile = ({ authenticated, setAuthenticated }) => {
   const dispatch = useDispatch();
   const params = useParams();
   const user = useSelector((state) => state.session.user);
-  const review = useSelector((state) => state.reviews[params.id]);
+  const rev = useSelector((state) => state.reviews[params.id]);
 
   return (
-    <div className='page-container'>
+    <div className='rev-profile-container'>
       <div>
-        <div className='container'>
-          {review && 
+        <div className='rev-profile'>
+          {rev &&
             <>
-                <div>
-                    {review.content}
-                </div>
-                <div>
-                    {review.score}
-                </div>
+              <div style={{ marginTop: '7px' }} className='order-title'>{rev.order_title}</div>
+              <div style={{ marginTop: '2px', fontSize: '18px' }} className='order-start'>{rev.order_start_time}</div>
+              <div>__________</div>
+              <div className='rev-data'>
+                <div style={{ marginBottom: '4px' }} className='rev-content'>"{rev.content}"</div>
+                <div className='rev-writer'>– {rev.writer.username}</div>
+                <div className='rev-score'>score: {rev.score}</div>
+              </div>
+              {(!rev.response_id && rev.writer_id != user.id) &&
+                <button className='blue-button' id={rev.application_id}>respond with review</button>
+              }
             </>
           }
         </div>
-        <div>
-            <Link to='/'>back to open orders</Link>
-          </div>
+        <div className='back-link-container'>
+          <Link className='back-link' to='/'>back to open orders</Link>
+        </div>
       </div>
     </div>
   );
