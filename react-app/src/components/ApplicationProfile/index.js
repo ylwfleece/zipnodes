afff02693c9556c9c84ace56150639f8dc1039f3
lettemax <maxlettenberger@gmail.com>
2021-03-09 15:53:49 -0600

    Styled app profile

--- react-app/src/components/ApplicationProfile/index.js
+++ react-app/src/components/ApplicationProfile/index.js
@@ -7,29 +7,58 @@ const ApplicationProfile = ({ authenticated, setAuthenticated }) => {
 
   const user = useSelector((state) => state.session.user);
   const params = useParams()
-  const application = useSelector((state) => state.applications[params.id]);
+  const app = useSelector((state) => state.applications[params.id]);
+
+  const confirmApp = (e) => {
+    // dispatch to update app to confirmed and order to inprogress
+    
+  }
+
+  const cancelApp = (e) => {
+    // dispatch to destroy app and order back to open
+  }
 
   return (
-    <div className='page-container'>
+    <div className='app-profile-container'>
       <div>
-        <div className='container'>
-          {application &&
+        <div className='app-profile'>
+          {app &&
             <>
-              <div>
-                {application.order_title}
+              <div className='order-title'>
+                <Link to={`/order/${app.order_id}`}>{app.order_title}</Link>
               </div>
-              <div>
-                {application.order_start_time}
+              <div className='order-start'>
+                {app.order_start_time}
               </div>
-              <div>
-                {application.status}
+              <div className='app-status'>
+                {(app.status == 'Pending') &&
+                  <>
+                    <p className='open-status'>status: {app.status}</p>
+                    <div className='link-container'>
+                      <Link className='cancel-link' to={`/`}>> cancel</Link>
+                    </div>
+                  </>
+                }
+                {(app.status == 'Accepted') &&
+                  <>
+                    <p className='pending-status'>application status: {app.status}</p>
+                    <div className='link-container'>
+                      <Link className='confirm-link' to={`/`} onClick={confirmApp}>> confirm</Link>
+                    </div>
+                    <div className='link-container'>
+                      <Link className='cancel-link' to={`/`} onClick={cancelApp}>> cancel</Link>
+                    </div>
+                  </>
+                }
+                {(app.status == 'Confirmed') &&
+                  <p className='complete-status'>{app.status}</p>
+                }
               </div>
-
             </>
           }
-          <div>
-            <Link to='/'>back to open orders</Link>
-          </div>
+        </div>
+        <div className='back-link-container'>
+          <Link className='back-link' to='/'>back to open orders</Link>
         </div>
       </div>
     </div>
